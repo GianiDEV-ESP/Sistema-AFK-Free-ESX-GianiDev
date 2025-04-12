0eedc3374990adb4430d743c7744219fd54b38f9
GianiDev-ESP <gianifiert@gmail.com>
2025-04-12 13:42:02 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,8 +3,3 @@ AddEventHandler("afk:setStatus", function(status, reason)
     local src = source
 end)
 
-RegisterNetEvent("afk:kickPlayer")
-AddEventHandler("afk:kickPlayer", function()
-    local src = source
-    DropPlayer(src, "Fuiste kickeado por inactividad (AFK más de 5 minutos sin usar /afk)")
-end)
